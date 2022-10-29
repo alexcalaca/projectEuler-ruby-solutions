59fb4c8c7e260694e0ac56a7615fc232bb6088b8
Alexandre Calaça <alexcalaca@gmail.com>
2022-10-29 01:45:06 -0300

    docs: update read me

---
+++ README.m
@@ -0,0 +1,58 @@
+<center><img src="https://user-images.githubusercontent.com/22925257/198814121-4684fafc-a965-4768-99ce-5055991f5e3f.png" width="300" height="350" align="center"></center>
+
+# Project Euler's solutions using Ruby
+
+> by AC Alexandre Calaça
+
+<br/>
+
+Description: Project Euler is a series of challenging mathematical/computer programming problems that will require more than just mathematical insights to solve. Although mathematics will help you arrive at elegant and efficient methods, the use of a computer and programming skills will be required to solve most problems.
+
+Link: https://projecteuler.net/about.
+
+___
+
+## Technologies used
+Ruby gems
+| Header	|  Header	|  Header	| Header 	| Header  	|
+| :-----:	|:---:	| :---:	| :---:	| :---:	|
+|  Rails	| webpacker 	| simple_form 	|  faker	|  action_text	|
+|  devise	| friendly_id 	|  	|  	|  	|
+|  	|  	|  	|  	|  	|
+
+
+ ___
+ 
+
+## Features
+- Feature 1;
+-
+
+___
+
+## Setup
+### Requirements
+- Requirement 1;
+- Requirement 2;
+
+### Usage
+1. Create the database
+> rails db:create
+
+2. Perform migrations
+> rails db:migrate
+
+3. Install gems and dependencies
+> bundle install
+> yarn install
+
+4. Populate the database
+> rails db:seed
+
+___
+
+
+## License
+
+It's available under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+
